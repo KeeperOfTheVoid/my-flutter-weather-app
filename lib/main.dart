64cc316b2a62726573c3447e7df4c2b32113d9b1
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2019-05-06 20:49:17 -0500

    Refactor Background Logic Into New Widget
    - Refactor background into new widget
    
    NOTE: This completes the Background section.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_weather_app/forecast/background/background_with_rings.dart';
 
 void main() => runApp(new MyApp());
 
@@ -58,142 +59,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: new Text(widget.title),
       ),
       body: new Stack(
-        fit: StackFit.expand,
         children: <Widget>[
-          new Image.asset(
-            'assets/weather-bk_enlarged.png',
-            fit: BoxFit.cover,
-          ),
-
-          new ClipOval(
-            clipper: new CircleClipper(
-              radius: 140.0,
-              offset: const Offset(40.0, 0.0),
-            ),
-            child: new Image.asset(
-              'assets/weather-bk.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          new CustomPaint(
-            painter: new WhiteCircleCutoutPainter(
-              centerOffset: const Offset(40.0, 0.0),
-              circles: [
-                new Circle(radius: 140.0, alpha: 0x10),
-                new Circle(radius: 140.0 + 15.0, alpha: 0x28),
-                new Circle(radius: 140.0 + 30.0, alpha: 0x38),
-                new Circle(radius: 140.0 + 75.0, alpha: 0x50),
-              ]
-            ),
-
-            child: new Container(),
-          ),
+          new BackgroundWithRings(),
         ],
       ),
     );
   }
-}
-
-class CircleClipper extends CustomClipper<Rect> {
-
-  final double radius;
-  final Offset offset;
-
-  CircleClipper({
-    this.radius,
-    this.offset = const Offset(0.0, 0.0),
-  });
-
-  @override
-  Rect getClip(Size size) {
-    return new Rect.fromCircle(
-      center: new Offset(0.0, size.height / 2) + offset,
-      radius: radius,
-    );
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper) {
-    return true;
-  }
-
-}
-
-class WhiteCircleCutoutPainter extends CustomPainter {
-
-  final Color overlayColor = const Color(0xFFAA88AA);
-
-  final List<Circle> circles;
-  final Offset centerOffset;
-  final Paint whitePaint;
-
-  WhiteCircleCutoutPainter({
-    this.circles = const[],
-    this.centerOffset = const Offset(0.0, 0.0),
-  }) : whitePaint = new Paint();
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (var i = 1; i < circles.length; ++i) {
-
-      _maskCircle(canvas, size, circles[i-1].radius);
-
-      whitePaint.color = overlayColor.withAlpha(circles[i - 1].alpha);
-
-      // Fill circle
-      canvas.drawCircle(
-          new Offset(0.0, size.height / 2) + centerOffset,
-          circles[i].radius,
-          whitePaint,
-      );
-    }
-
-    // Mask area of final circle
-    _maskCircle(canvas, size, circles.last.radius);
-
-    // Draw an overlay that fills the rest of the screen
-    whitePaint.color = overlayColor.withAlpha(circles.last.alpha);
-    canvas.drawRect(
-        new Rect.fromLTWH(0.0, 0.0, size.width, size.height),
-        whitePaint
-    );
-  }
-
-  _maskCircle(Canvas canvas, Size size, double radius) {
-    Path clippedCircle = new Path();
-
-    // If you want to clip a path, you need to start with a larger space.
-    // Start with the entire screen, then cut out a circle we want to mask.
-    clippedCircle.fillType = PathFillType.evenOdd;
-    clippedCircle.addRect(new Rect.fromLTWH(0.0, 0.0, size.width, size.height));
-    clippedCircle.addOval(
-      new Rect.fromCircle(
-        center: new Offset(0.0, size.height / 2) + centerOffset,
-        radius: radius,
-      ),
-    );
-    
-    canvas.clipPath(clippedCircle);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
-
-}
-
-class Circle {
-
-  final double radius;
-  final int alpha;
-
-  Circle({
-    this.radius,
-    this.alpha = 0xFF,
-  });
-
-
-
 }
