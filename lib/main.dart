a84914885daf650d71007073b9599539b4b966e2
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2019-05-07 22:26:08 -0500

    Refactor Sliding Drawer Logic
    - Refactor Sliding Drawer logic into separate widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_weather_app/forecast/app_bar.dart';
 import 'package:my_weather_app/forecast/background/background_with_rings.dart';
 import 'package:my_weather_app/forecast/week_drawer.dart';
+import 'package:my_weather_app/generic_widgets/sliding_drawer.dart';
 
 void main() => runApp(new MyApp());
 
@@ -83,108 +84,16 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
               onDrawerArrowTap: openableController.open,
             ),
           ),
-          new Stack(
-            children: <Widget>[
-              new GestureDetector(
-                onTap: openableController.isOpen()
-                    ? openableController.close
-                    : null
-              ),
-              new Transform(
-                transform: new Matrix4.translationValues(
-                  125.0 * (1.0 - openableController.percentOpen), // To the right
-                  0.0,
-                  0.0,
-                ),
-                child: new Align(
-                  alignment: Alignment.centerRight,
-                  child: new WeekDrawer(
-                    onDaySelected: (String title) {
-                      openableController.close();
-                    },
-                  ),
-                ),
-              ),
-            ],
+          new SlidingDrawer(
+            openableController: openableController,
+            drawer: new WeekDrawer(
+              onDaySelected: (String title) {
+                openableController.close();
+              },
+            ),
           ),
         ],
       ),
     );
   }
-}
-
-class OpenableController extends ChangeNotifier {
-
-  OpenableState _state = OpenableState.closed;
-  AnimationController _opening;
-
-  OpenableController({
-    @required TickerProvider vsync,
-    @required Duration openDuration,
-  }) : _opening = new AnimationController(duration: openDuration, vsync: vsync) {
-    _opening
-      ..addListener(notifyListeners) // Gets called everytime frame changes
-      ..addStatusListener((AnimationStatus status) {
-        // Gets called only when animation starts/stops
-        switch(status) {
-          case AnimationStatus.forward:
-            _state = OpenableState.opening;
-            break;
-          case AnimationStatus.completed:
-            _state = OpenableState.open;
-            break;
-          case AnimationStatus.reverse:
-            _state = OpenableState.closing;
-            break;
-          case AnimationStatus.dismissed:
-            _state = OpenableState.closed;
-            break;
-        }
-        notifyListeners();
-      });
-  }
-
-  get state => _state;
-
-  get percentOpen => _opening.value;
-
-  bool isOpen() {
-    return _state == OpenableState.open;
-  }
-
-  bool isOpening() {
-    return _state == OpenableState.opening;
-  }
-
-  bool isClosed() {
-    return _state == OpenableState.closed;
-  }
-
-  bool isClosing() {
-    return _state == OpenableState.closing;
-  }
-
-  void open() {
-    _opening.forward();
-  }
-
-  void close() {
-    _opening.reverse();
-  }
-
-  void toggle() {
-    if(isClosed()) {
-      open();
-    } else if(isOpen()) {
-      close();
-    }
-  }
-
-}
-
-enum OpenableState {
-  closed,
-  opening,
-  open,
-  closing,
 }
